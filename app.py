fc9950b927c0347be73c1a6962eaef56462f4277
1665169869 <1665169869@qq.com>
2022-01-03 13:41:21 +0800

    1.0.0

---
+++ app.py
@@ -0,0 +1,51 @@
+from flask import Flask, render_template, redirect
+app = Flask(__name__, static_url_path='')
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/user')
+def user():
+    return render_template('user.html')
+
+
+@app.route('/online')
+def online():
+    return render_template('online.html')
+
+
+@app.route('/passwd')
+def passwd():
+    return render_template('passwd.html')
+
+
+@app.route('/login')
+def login():
+    return redirect('/login/password', 302)
+
+
+@app.route('/login/')
+def login_():
+    return redirect('/login/password', 302)
+
+
+@app.route('/login/password')
+def login_password():
+    return render_template('login/password.html')
+
+
+@app.route('/login/send')
+def login_send():
+    return render_template('login/send.html')
+
+
+@app.errorhandler(404)
+def page_not_found(error=None):
+    return redirect('/login', 302)
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ main.py
@@ -0,0 +1,28 @@
+import sys
+
+from PyQt5.QtCore import *
+from PyQt5.QtWebEngineWidgets import *
+from PyQt5.QtWidgets import *
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.browser = QWebEngineView()
+
+    def initUI(self, url=None):
+        self.setWindowTitle('糕糕')  # 窗口标题
+        self.resize(415, 550)  # 窗口的大小和位置设置
+        # self.setWindowFlags(Qt.FramelessWindowHint)  # 关闭标题栏
+        self.setWindowFlags(Qt.Drawer | Qt.WindowCloseButtonHint)  # 去掉左上角图标 | 只显示关闭按钮
+        # 加载外部的web界面
+        self.browser.load(QUrl(url))
+        self.setCentralWidget(self.browser)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    win = MainWindow()
+    win.initUI('http://127.0.0.1:5000/login')
+    win.show()
+    app.exit(app.exec_())
