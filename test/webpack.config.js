b24ebc8986794ca695f315675456371c4134ab35
baiyu16 <1665169869@qq.com>
2022-01-21 18:09:30 +0800

    删除文件

---
+++ test/webpack.config.js
@@ -0,0 +1,9 @@
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js'
+    },
+    mode: 'development' // 设置mode
+}
