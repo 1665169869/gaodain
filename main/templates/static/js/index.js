e3ad36af6d2ceb2c335f930420275d1e99929d2c
1665169869 <1665169869@qq.com>
2022-02-17 02:36:48 +0800

    新增了验证码登录和获取验证码函数，并修改了login.html

--- main/templates/static/js/index.js
+++ main/templates/static/js/index.js
@@ -1,61 +1,81 @@
 
-function request(settings){
+function request(settings) {
     // name, data, Authorization = null, async = true, success
     let defaultSettings = {
         name: null,
         data: {},
         Authorization: null,
         async: true,
-        success: function(data, status, xhr) {
+        success: function (data, status, xhr) {
             console.log(data, status, xhr);
         }
-    } 
+    }
 
     $.extend(defaultSettings, settings)
 
     // let url = config.API.service + config.API[name].url;
-	let url = API.service + API[defaultSettings.name].url;
+    let url = API.service + API[defaultSettings.name].url;
     let headers = {
         "content-type": "application/json",
         "Authorization": defaultSettings.Authorization,
     }
     // let method = config.API[name].method;
     let method = API[defaultSettings.name].method;
-	let res;
+    let res;
 
-    if (method == "GET" || method == "get"){
+    if (method == "GET" || method == "get") {
         res = $.get({
             url: url,
             data: defaultSettings.data,
             async: defaultSettings.async
-        }, function(data, status, xhr){
+        }, function (data, status, xhr) {
             defaultSettings.success(data, status, xhr);
         });
-    } else if (method == "POST" || method == "post"){
+    } else if (method == "POST" || method == "post") {
         res = $.post({
             url: url,
             data: defaultSettings.data,
             async: defaultSettings.async
-        }, function(data, status, xhr){
+        }, function (data, status, xhr) {
             defaultSettings.success(data, status, xhr);
         });
     }
     return res;
 };
 
-function login(data){
+function login(data) {
     let res = request({
         name: "login",
         data: data,
-        success: function(data, status, xhr){
+        success: function (data, status, xhr) {
             // 这里写回调函数
-            console.log(data, status, xhr);
+            // console.log(data, status, xhr);
         }
     });
+    return res;
     // console.log(res.status, res.responseJSON, res);
 };
 
-login({
-    mobile: "手机号",
-    password: "密码"
-});
+function smsLogin(data) {
+    let res = request({
+        name: "smsLogin",
+        data: data,
+        success: function (data, status, xhr) {
+            // 这里写回调函数
+            // console.log(data, status, xhr);
+        }
+    });
+    return res;
+};
+
+function sendNote(data) {
+    let res = request({
+        name: "sendNote",
+        data: data,
+        success: function (data, status, xhr) {
+            // 这里写回调函数
+            // console.log(data, status, xhr);
+        }
+    });
+    return res;
+};
