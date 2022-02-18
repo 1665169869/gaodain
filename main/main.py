1b0f60efaeb4b9de614681c781765fef960ee1be
1665169869 <1665169869@qq.com>
2022-02-19 00:19:48 +0800

    新增安装包 3.0成功完工

--- main/main.py
+++ main/main.py
@@ -2,34 +2,48 @@ import sys
 import server
 import _thread
 import os
-from PyQt5.QtCore import *
-from PyQt5.QtWebEngineWidgets import *
-from PyQt5.QtWidgets import *
+import webbrowser
+import time
+# from PyQt5.QtCore import *
+# from PyQt5.QtWebEngineWidgets import *
+# from PyQt5.QtWidgets import *
 
 
-class MainWindow(QMainWindow):
-    def __init__(self):
-        super(MainWindow, self).__init__()
-        self.browser = QWebEngineView()
-        self.host = "127.0.0.1"
-        self.port = 55581
-    def initUI(self):
-        self.setWindowTitle('糕糕')  # 窗口标题
-        self.resize(415, 550)  # 窗口的大小和位置设置
-        # self.setWindowFlags(Qt.FramelessWindowHint)  # 关闭标题栏
-        self.setWindowFlags(Qt.WindowCloseButtonHint)  # 去掉左上角图标 | 只显示关闭按钮
-        # 加载外部的web界面
-        self.browser.load(QUrl(f"http://{self.host}:{self.port}/"))
-        self.setCentralWidget(self.browser)
-        os.chdir(os.path.split(os.path.realpath(__file__))[0] + "\\assets")
-        _thread.start_new_thread(server.server_dir, (self.host, self.port))
-        _thread.start_new_thread(server.server_api, (self.host, self.port + 1))
+# class MainWindow(QMainWindow):
+#     def __init__(self):
+#         super(MainWindow, self).__init__()
+#         self.browser = QWebEngineView()
+#         self.host = "127.0.0.1"
+#         self.port = 55581
+#     def initUI(self):
+#         self.setWindowTitle('糕糕')  # 窗口标题
+#         self.resize(415, 550)  # 窗口的大小和位置设置
+#         # self.setWindowFlags(Qt.FramelessWindowHint)  # 关闭标题栏
+#         self.setWindowFlags(Qt.WindowCloseButtonHint)  # 去掉左上角图标 | 只显示关闭按钮
+#         # 加载外部的web界面
+#         self.browser.load(QUrl(f"http://{self.host}:{self.port}/"))
+#         self.setCentralWidget(self.browser)
+#         os.chdir(os.path.split(os.path.realpath(__file__))[0] + "\\assets")
+#         _thread.start_new_thread(server.server_dir, (self.host, self.port))
+#         _thread.start_new_thread(server.server_api, (self.host, self.port + 1))
     
+
+# if __name__ == '__main__':
+#     app = QApplication(sys.argv)
+#     win = MainWindow()
+    
+#     win.initUI()
+#     win.show()
+#     app.exit(app.exec_())
+
 
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    win = MainWindow()
-    
-    win.initUI()
-    win.show()
-    app.exit(app.exec_())
+    host = "127.0.0.1"
+    port = 55581
+    os.chdir(os.path.split(os.path.realpath(__file__))[0] + "\\assets")
+    _thread.start_new_thread(server.server_dir, (host, port))
+    _thread.start_new_thread(server.server_api, (host, port + 1))
+    webbrowser.open(f"http://{host}:{port}/")
+    while 1:
+        time.sleep(9999)
+        pass
---
+++ main/糕点安装包制作.iss
@@ -0,0 +1,58 @@
+; 脚本由 Inno Setup 脚本向导 生成！
+; 有关创建 Inno Setup 脚本文件的详细资料请查阅帮助文档！
+
+#define MyAppName "糕点"
+#define MyAppVersion "3.0.0"
+#define MyAppPublisher "白羽"
+#define MyAppURL "https://github.com/1665169869/gaodain"
+#define MyAppExeName "糕点.exe"
+
+[Setup]
+; 注: AppId的值为单独标识该应用程序。
+; 不要为其他安装程序使用相同的AppId值。
+; (若要生成新的 GUID，可在菜单中点击 "工具|生成 GUID"。)
+AppId={{E638F3E1-3BE6-4C09-878B-F216C4DC76AC}
+AppName={#MyAppName}
+AppVersion={#MyAppVersion}
+;AppVerName={#MyAppName} {#MyAppVersion}
+AppPublisher={#MyAppPublisher}
+AppPublisherURL={#MyAppURL}
+AppSupportURL={#MyAppURL}
+AppUpdatesURL={#MyAppURL}
+DefaultDirName={autopf}\{#MyAppName}
+DefaultGroupName={#MyAppName}
+AllowNoIcons=yes
+; [Icons] 的“quicklaunchicon”条目使用 {userappdata}，而其 [Tasks] 条目具有适合 IsAdminInstallMode 的检查。
+UsedUserAreasWarning=no
+; 以下行取消注释，以在非管理安装模式下运行（仅为当前用户安装）。
+;PrivilegesRequired=lowest
+PrivilegesRequiredOverridesAllowed=dialog
+OutputDir=F:\16651\Source code\python\gaogao\dist
+OutputBaseFilename=糕点 -- 第三方糕糕电脑客户端安装包
+SetupIconFile=F:\16651\Source code\python\gaogao\main\assets\favicon.ico
+Compression=lzma
+SolidCompression=yes
+WizardStyle=modern
+
+[Languages]
+Name: "chinesesimp"; MessagesFile: "compiler:Default.isl"
+
+[Tasks]
+Name: "desktopicon"; Description: "{cm:CreateDesktopIcon}"; GroupDescription: "{cm:AdditionalIcons}"; Flags: unchecked
+Name: "quicklaunchicon"; Description: "{cm:CreateQuickLaunchIcon}"; GroupDescription: "{cm:AdditionalIcons}"; Flags: unchecked; OnlyBelowVersion: 6.1; Check: not IsAdminInstallMode
+
+[Files]
+Source: "F:\16651\Source code\python\gaogao\dist\糕点 -- 第三方糕糕电脑客户端\糕点.exe"; DestDir: "{app}"; Flags: ignoreversion
+Source: "F:\16651\Source code\python\gaogao\dist\糕点 -- 第三方糕糕电脑客户端\*"; DestDir: "{app}"; Flags: ignoreversion recursesubdirs createallsubdirs
+; 注意: 不要在任何共享系统文件上使用“Flags: ignoreversion”
+
+[Icons]
+Name: "{group}\{#MyAppName}"; Filename: "{app}\{#MyAppExeName}"
+Name: "{group}\{cm:ProgramOnTheWeb,{#MyAppName}}"; Filename: "{#MyAppURL}"
+Name: "{group}\{cm:UninstallProgram,{#MyAppName}}"; Filename: "{uninstallexe}"
+Name: "{autodesktop}\{#MyAppName}"; Filename: "{app}\{#MyAppExeName}"; Tasks: desktopicon
+Name: "{userappdata}\Microsoft\Internet Explorer\Quick Launch\{#MyAppName}"; Filename: "{app}\{#MyAppExeName}"; Tasks: quicklaunchicon
+
+[Run]
+Filename: "{app}\{#MyAppExeName}"; Description: "{cm:LaunchProgram,{#StringChange(MyAppName, '&', '&&')}}"; Flags: nowait postinstall skipifsilent
+
